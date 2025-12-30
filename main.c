5637142026c030c56bd591c1c37e4ac371b28b81
nicolovinci <12999303@itisrossi.vi.it>
2025-12-30 17:46:25 +0100

    Created pipe and made forks.

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int fileDescriptor[2];
+    if (pipe(fileDescriptor) == -1) {
+        perror("pipe");
+        exit(-1);
+    }
+    pid_t Elf1;
+    if ((Elf1 = fork()) == -1) {
+        perror("fork");
+        exit(-1);
+    }
+    if (Elf1 == 0) {
+
+    } else {
+        pid_t Elf2;
+        if ((Elf2 = fork()) == -1) {
+            perror("fork");
+            exit(-1);
+        }
+        if (Elf2 == 0) {
+
+        } else {
+
+        }
+    }
     return 0;
 }
