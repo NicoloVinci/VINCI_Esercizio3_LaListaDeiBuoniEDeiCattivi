a3028a04173f163939845556dcec3305b9a160f2
nicolovinci <12999303@itisrossi.vi.it>
2025-12-31 16:44:44 +0100

    Done the first point of the To-Do list.

--- main.c
+++ main.c
@@ -24,7 +24,21 @@ int main(void) {
         if (Elf2 == 0) {
 
         } else {
-
+            int valido;
+            char c;
+            do {
+                printf("Inserisci il numero di bambini: ");
+                int input;
+                if (scanf("%d", &input) != 1 || input < -1) {
+                    valido = 0;
+                }
+                do {
+                    c = getchar();
+                    if (c != '\n' && c != ' ' && c != '\t') {
+                        valido = 0;
+                    }
+                } while (c != '\n' && c != EOF);
+            } while (!valido);
         }
     }
     return 0;
